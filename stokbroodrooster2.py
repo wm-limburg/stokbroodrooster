1561687dd57939518c5470ac210ab9d064796485
JellyPuddy <43268724+JellyPuddy@users.noreply.github.com>
2018-09-15 15:17:21 +0200

    Update stokbroodrooster2.py
    Nutteloze comments en andere zooi weggehaald

--- stokbroodrooster2.py
+++ stokbroodrooster2.py
@@ -7,7 +7,6 @@ for d in data:
 	print(d)
 
 begindag = cont[cont.find(':')+2:cont.find(' ',cont.find(':')+3)]
-#print(begindag)
 
 dagen = ['maandag', 'dinsdag', 'woensdag', 'donderdag', 'vrijdag']
 
@@ -17,37 +16,23 @@ namen = [d.pop(0) for d in data]
 alles = []
 
 for i in range(5):
-	#i += ind
-	while i > 4: i -= 5
-	#print(dagen[i])
 	namenopdag = []
 	for uur in range(1,8):
 		namenopuur = []
 		for j,naam in enumerate(namen):
-			#print(j,naam)
 			persoon = data[j]
-			#input(persoon)
 			if str(uur) in data[j][i]:
 				namenopuur += [naam]
-				#print(data[j])
-				#print(data[j][i])
-				#print(naam)
-				#print(uur)
-				#input()
 		if not namenopuur == []:
 			namenopdag += [(namenopuur,uur)]
 	alles += [(namenopdag, dagen[i+ind if i+ind<=4 else i+ind-5])]
-
-#[print(dagg[-1], *dagg[:-1]) for dagg in alles]
 
 for dagg in alles:
 	uren = []
 	dag = dagg[-1]
 	dagg = [*dagg[0]]
 	print('\n'+dag)
-	#print(dagg)
 	uren = [uurr[1] for uurr in dagg]
 	for i,uur in enumerate(uren):
 		print(str(uur)+'e uur:')
 		print(*dagg[i][0])
-		#input()
